fb599af90a39a0c7a9a49924cdfbd11cacc43a45
chandan kumar <chandan.kumar.eee15@itbhu.ac.in>
2026-05-31 18:19:30 +0530

    added mutext

---
+++ system_grinding/mutex/Main.java
@@ -0,0 +1,38 @@
+public class Main {
+    public static void main(String[] args) throws InterruptedException {
+        MyMutex mutex = new MyMutex();
+
+        Thread threadA = new Thread(() -> work(mutex), "Thread-A");
+        Thread threadB = new Thread(() -> work(mutex), "Thread-B");
+        Thread threadC = new Thread(() -> work1(mutex), "Thread-C");
+
+        threadA.start();
+        threadB.start();
+        threadC.start();
+
+        threadA.join();
+        threadB.join();
+        threadC.join();
+
+        System.out.println("Done.");
+    }
+    
+    static void work1(MyMutex mutex) {
+        try {
+            mutex.printer();
+        } catch (Exception e) {
+            Thread.currentThread().interrupt();
+        }
+    }
+    static void work(MyMutex mutex) {
+        try {
+            mutex.lock();
+            System.out.println(Thread.currentThread().getName() + " acquired lock");
+            Thread.sleep(5000);
+            mutex.unlock();
+            System.out.println(Thread.currentThread().getName() + " released lock");
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+    }
+}
