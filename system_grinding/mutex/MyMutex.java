cb18a6fd3ff107e2f3365cc163ca2e81ca128170
chandan kumar <chandan.kumar.eee15@itbhu.ac.in>
2026-05-31 18:09:46 +0530

    feat: add MyMutex with lock/unlock using wait/notifyAll

---
+++ system_grinding/mutex/MyMutex.java
@@ -0,0 +1,15 @@
+public class MyMutex {
+    private boolean locked = false;
+
+    public synchronized void lock() throws InterruptedException {
+        while (locked) {
+            wait();
+        }
+        locked = true;
+    }
+
+    public synchronized void unlock() {
+        locked = false;
+        notifyAll();
+    }
+}
